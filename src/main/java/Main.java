d0ccf5369af6739dd4592965df6329a0b8e0c5aa
satker <satker73@gmail.com>
2018-05-13 13:07:42 +0300

    Remove unnecessary variable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,9 @@
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import service.emulation.EmulationServiceImpl;
 
 public class Main {
 
   public static void main(String[] args) {
-    ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+    new ClassPathXmlApplicationContext(
         "WEB-INF/applicationContext.xml");
-    EmulationServiceImpl emulation = (EmulationServiceImpl) applicationContext.getBean(
-        "emulation");
-    emulation.start();
   }
-
 }
